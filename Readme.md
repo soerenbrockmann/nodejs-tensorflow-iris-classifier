52821befbe6725011b5ed411fcad315767c00c43
SBROCKMANN.COM <sbrockmann.com@sb.local>
2020-11-18 16:14:09 +0100

    Add comments

--- Readme.md
+++ Readme.md
@@ -7,3 +7,10 @@ Run `npm install`
 ## Start
 
 Run `npm run start`
+
+
+## YouTube Tutorial
+
+Part 1: https://youtu.be/kQ7AchCCC3s
+Part 2: https://youtu.be/bLq7qsvw_bI
+Part 3: https://youtu.be/tUDbFRbHma8
--- index.js
+++ index.js
@@ -27,9 +27,18 @@ async function start() {
       return { xs: Object.values(xs), ys: Object.values(labels) };
     })
     .batch(10);
+  console.log('Data', await trainData.toArray());
+  console.log('Data', await data.toArray());
+
+  // At the top are the four features, in the middle is a hidden layer with five nodes, 
+  // and at the bottom of the three nodes that we'll use for the classification. 
 
   // 2. Set model
   const model = tf.sequential();
+
+  // Input features = Sigmoid (0-1 prediction)
+  // Then we add the hidden layer with five neurons. By specifying the input shape with the number of features, 
+  // which is calculated to be four
   model.add(
     tf.layers.dense({
       inputShape: [numOfFeatures],
@@ -38,6 +47,10 @@ async function start() {
     })
   );
 
+  // The softmax function is a more generalized logistic activation function which is used for multiclass classification.
+  // Outputs are 3 classes: setosa, virgincia, versicolor
+  // Then we add the three neurons at the bottom activating them with a Softmax function to get the probability that the 
+  // pattern will match the neuron for that class of flower
   model.add(
     tf.layers.dense({
       activation: "softmax",
@@ -45,6 +58,11 @@ async function start() {
     })
   );
 
+  // Computes the crossentropy loss between the labels and predictions.
+  // Adam optimization is a stochastic gradient descent method that is based on adaptive estimation of first-order and second-order moments.
+  // Learning rate
+  // Adam is a replacement optimization algorithm for stochastic gradient descent for training deep learning models. Adam combines the best 
+  // properties of the AdaGrad and RMSProp algorithms to provide an optimization algorithm that can handle sparse gradients on noisy problems.
   model.compile({
     loss: "categoricalCrossentropy",
     optimizer: tf.train.adam(0.06),
